7de50e957dbc15be6fa3e581c1af2542074a5ad0
kwabenasapong <kwabenasapong@gmail.com>
2022-11-30 13:27:37 +0000

    Try challenge

---
+++ javascript/challenge/c_2.js
@@ -0,0 +1,31 @@
+#!/usr/bin/node
+const converter = require('./10-converter').converter;
+
+function checkArg (x, base) {
+  const number = parseInt(x, base);
+  if (isNaN(number)) { return 0; }
+  return number;
+}
+
+if (process.argv.length <= 2 || checkArg(process.argv[2], 10) === 0) {
+  console.log('Missing number of occurrences');
+  process.exit(1);
+} else {
+  const x = process.argv.length;
+  const len = parseInt(x) - 2;
+  //console.log(len);
+  //console.log(parseInt(process.argv[2]));
+  let i = 2;
+  let myConverter_10 = converter(10);
+  let myConverter_16 = converter(6);
+  while (i < len) {
+    let number = myConverter_10(parseInt(process.argv[i], 2));
+    if (i === 2) {console.log(typeof(number));}
+    if (i !== (len - 1)) {
+      process.stdout.write(myConverter_16(parseInt(number)) + ', ');
+    } else {
+      process.stdout.write(myConverter_16(parseInt(number)));
+    }
+    i++;
+  }
+}
