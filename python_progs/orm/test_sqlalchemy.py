ef7a7204672f73335a806bdf5e42bdce1c903800
kwabenasapong <kwabenasapong@gmail.com>
2022-12-06 17:24:26 +0000

    orm files to finish

---
+++ python_progs/orm/test_sqlalchemy.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+'''
+Learning to use sqlalcemy the ALX way
+
+import MySQLdb
+from sys import argv
+
+db = MySQLdb.connect(host='localhost', user='koby', passwd='Kw@5a9ng', db='my_self')
+cur = db.cursor()
+cur.execute("CREATE TABLE IF NOT EXISTS member (id INT UNSIGNED PRIMARY KEY AUTO_INCREMENT, name VARCHAR(256), age INT UNSIGNED)")
+members = ('Kwabena Sapong', 'Alice Sapong', 'Afrakoma Sapong')
+for member in members:
+    cur.execute("INSERT INTO member (name) VALUES (%s)", (member,))
+    print("ID: %s" % cur.lastrowid)
+cur.execute("INSERT INTO personal (name, age) values ('Alice Sapong', 33)")
+
+cur.execute("SELECT * FROM member")
+rows = cur.fetchall()
+for row in rows:
+    for col in row:
+        print("{}".format(col))
+    print('----')
+
+cur.execute("SELECT * FROM personal WHERE id = 1")
+row = cur.fetchone()
+print("id: {}, name: {}, age: {}".format(row[0], row[1], row[2]))
+cur.close()
+db.commit()
+db.close()
+'''
+
+import sqlalchemy
+from sys import argv
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+
+
+#if __name__ == "__main__":
+'''connecting to the mysql'''
+#url = 'mysql://{}:{}@localhost/{}'.format("koby", "Kw@5a9ng", "my_self")
+engine = create_engine('mysql://{}:{}@localhost/{}'.format("koby", "Kw@5a9ng", "my_self"), pool_pre_ping=True)
+
+'''Mapping the ORM'''
+Base = declarative_base()
+
+class Member(Base):
+    __tablename__ = 'members'
+
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+    age = Column(Integer)
+
+    def __repr__(self):
+        return "Member_{} (name= {}, age= {})".format(self.id, self.name, self.age)
+
+'''Create a Schema'''
+#print(Member.__table__)
+
+Base.metadata.create_all(engine)
+
+
+
