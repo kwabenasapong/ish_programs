4667d9a644975c35513101f8624baaa01c0a82d0
kwabenasapong <kwabenasapong@gmail.com>
2023-01-25 09:45:43 +0000

    Add jquery dir

--- javascript/test_2/app.js
+++ javascript/test_2/app.js
@@ -1,33 +1,33 @@
 #!/usr/bin/node
-//THE SIMPLEST MODULE
+// THE SIMPLEST MODULE
 require('./hello.js');
 
-//PATTERN 1: DEFINE A GLOBAL
+// PATTERN 1: DEFINE A GLOBAL
 require('./foo.js');
 foo();
 
-//PATTERN 2: EXPORT AN ANONYMOUS FUNCTION
-var bar = require('./bar.js');
+// PATTERN 2: EXPORT AN ANONYMOUS FUNCTION
+const bar = require('./bar.js');
 bar();
 
-//PATTERN 3: EXPORT A NAMED FUNCTION
-var fiz = require('./fiz.js').fiz;
+// PATTERN 3: EXPORT A NAMED FUNCTION
+const fiz = require('./fiz.js').fiz;
 fiz();
 
-//PATTERN 4: EXPORT AN ANONYMOUS OBJECT
-var buz = require('./buz.js')
+// PATTERN 4: EXPORT AN ANONYMOUS OBJECT
+const buz = require('./buz.js');
 buz.log();
 
-//PATTERN 5: EXPORT A NAMED OBJECT
-var baz = require('./baz.js').Baz;
+// PATTERN 5: EXPORT A NAMED OBJECT
+const baz = require('./baz.js').Baz;
 baz.log();
 
-//PATTERN 6: EXPORT AN ANONYMOUS PROTOTYPE
-var Doo = require('./doo.js');
-var doo = new Doo();
+// PATTERN 6: EXPORT AN ANONYMOUS PROTOTYPE
+const Doo = require('./doo.js');
+const doo = new Doo();
 doo.log();
 
-//PATTERN 7: EXPORT A NAMED PROTOTYPE
-var Qux = require('./qux.js').Qux;
-var qux = new Qux();
+// PATTERN 7: EXPORT A NAMED PROTOTYPE
+const Qux = require('./qux.js').Qux;
+const qux = new Qux();
 qux.log();
